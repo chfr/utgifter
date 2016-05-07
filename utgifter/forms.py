375625692eab9f2ec477709a65a792f7145dffa0
Christoffer Friberg <stoff3+github@gmail.com>
2016-05-07 22:07:32 +0200

    Made the searchstring field optional when creating new matchers

--- utgifter/forms.py
+++ utgifter/forms.py
@@ -4,7 +4,7 @@ from .models import Matcher, Tag
 
 
 class MatcherForm(forms.ModelForm):
-    searchstrings = forms.CharField(widget=forms.Textarea(attrs={"cols": 20, "rows": 3}))
+    searchstrings = forms.CharField(required=False, widget=forms.Textarea(attrs={"cols": 20, "rows": 3}))
 
     class Meta:
         model = Matcher
